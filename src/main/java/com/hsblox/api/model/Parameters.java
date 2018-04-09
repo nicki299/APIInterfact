ec654ba00f618f5d5ecf27e5cade63a32fba3d80
peeush.jaiswal <peeush.jaiswal@bloxshare.com>
2018-04-09 11:08:51 -0400

    Testing

--- src/main/java/com/hsblox/api/model/Parameters.java
+++ src/main/java/com/hsblox/api/model/Parameters.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * Created by devops on 4/6/18.
+ * Created by devops on 4/6/18. This can be created
  */
 @Entity
 @Table(name = "PARAMETERS")
